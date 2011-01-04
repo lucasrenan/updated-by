278600f388a13f3e9221ae105dfd8ca3bcffd8e8
Lucas Renan <lucas@freelancersbrasil.com>
2011-01-04 14:50:10 -0200

    create gem skeleton

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '8bb5462c58c21fcea3f2a9fd4ea13f35107341104651492b08cc69d52f771ae553c9e7c8a2e39815962a101c17c2de91137c261cf96fc5118e842a44f52b336a'
---
+++ test/updated_by_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UpdatedByTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, UpdatedBy
+  end
+end
